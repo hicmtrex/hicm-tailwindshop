5fee812a0569b4976180e989522030458a0a89e1
Firstname Lastname <your_email@youremail.com>
2022-02-25 00:56:43 +0100

    pushing

--- components/navbar.js
+++ components/navbar.js
@@ -96,7 +96,6 @@ const Navbar = () => {
                 {!user ? (
                   <a
                     href='/api/auth/login'
-                    type='button'
                     className='bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
                   >
                     Login
